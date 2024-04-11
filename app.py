ec28f6cb7a53a9f925307077dce9337852f6f0a1
Valpal84 <142853655+Valpal84@users.noreply.github.com>
2024-04-10 20:34:59 -0500

    update app.py for work in progress

--- app.py
+++ app.py
@@ -1,1 +1,140 @@
+# Add imports
+from shiny import reactive, render
+from shiny.express import ui
+from shiny.express import input
+import random
+from datetime import datetime
+from faicons import icon_svg
+from collections import deque
+import pandas as pd
+import plotly.express as px
+from shinywidgets import render_plotly
+from scipy import stats
+import seaborn as sns
+from shinyswatch import theme
 
+# Load diamonds dataset into a df
+diamonds_df = sns.load_dataset('diamonds')
+
+UPDATE_INTERVAL_SECS: int = 2
+
+# Add a deque
+DEQUE_SIZE: int = 8
+reactive_value_wrapper = reactive.value(deque(maxlen=DEQUE_SIZE))
+
+@reactive.calc()
+def reactive_calc_combined():
+    reactive.invalidate_later(UPDATE_INTERVAL_SECS)
+    
+    # Data generation logic
+    cost = round(random.uniform(100, 120), 2)
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    new_dictionary_entry = {"cost":cost, "timestamp":timestamp}
+
+    #Get the deque and append the new entry
+    reactive_value_wrapper.get().append(new_dictionary_entry)
+
+     #Get a snapshot of the current deque for any further processing
+    deque_snapshot = reactive_value_wrapper.get()
+
+    #For display Convert deque to dataframe for display
+    df = pd.DataFrame(deque_snapshot)
+
+    #For display-get the latest dictionary entry
+    latest_dictionary_entry = new_dictionary_entry
+
+    #Return a tuple with everything we need
+    return deque_snapshot, df, latest_dictionary_entry
+
+# Adding a page title
+ui.page_opts(title="Valerie's Diamond Dashboard", fillable=True)
+
+# Adding a page theme
+theme.vapor()
+
+# Adding a sidebar and sidebar components
+with ui.sidebar(open="open"):
+    ui.h3("Facts About Diamonds", class_="text-center", style="color:navy")
+    ui.p(
+        "A demonstration of real-time costs for sourced diamonds.",
+        class_="text-center",
+    )
+    ui.input_numeric("plotly_bin_count", "Bin_count", 1, min=1, max=15)
+    ui.input_checkbox_group("select_diamond_cut",
+                           "Select Diamond Cut",
+                           ["Premium", "Ideal", "Very Good", "Good", "Fair"],
+                           selected=["Very Good"],
+                           inline=True,
+                           )
+    ui.input_slider("carat", "Carat", 0,2.5, 1)
+    ui.hr()
+    ui.h6("Helpful Links:")
+    ui.a(
+        "Valerie's GitHub Source",
+        href="https://github.com/Valpal84/cintel-05-cintel", 
+        target="_blank",
+    )
+    
+    ui.a("PyShiny", href="https://shiny.posit.co/py/", target="_blank")
+    ui.a(
+        "Seaborn's Diamond Dataset",
+        href="https://github.com/mwaskom/seaborn-data/blob/master/diamonds.csv",
+        target="_blank",
+    )
+
+# Main content area
+
+ui.h2("Current Cost", style="color:navy")
+
+with ui.layout_columns():
+    with ui.value_box(
+        showcase=icon_svg("wallet"),
+        theme="bg-gradient-blue-teal",
+    ):
+      
+        "Current Diamond Cost"
+        
+        @render.text
+        def display_temp():
+            deque_snapshot, df, latest_dictionary_entry = reactive_calc_combined()
+            theme="bg-gradient-green-blue",
+            return f"{latest_dictionary_entry['cost']} $"
+
+
+        "Higher than average costs"
+
+    with ui.card(full_screen=True):
+        ui.card_header("Current Date and Time", style="color:navy")
+        @render.text
+        def display_time():
+            deque_snapshot, df, latest_dictionary_entry = reactive_calc_combined()
+            theme="bg-gradient-green-blue",
+            return f"{latest_dictionary_entry['timestamp']}"
+
+with ui.layout_columns():
+    with ui.navset_card_pill(id="tab1"):
+        with ui.nav_panel("Diamond Data Table"):
+
+            @render.data_frame
+            def penguins_data_table():
+                return render.DataTable(diamonds_df)
+
+        with ui.nav_panel("Diamond Data Grid"):
+
+            @render.data_frame
+            def penguins_data_grid():
+                return render.DataGrid(diamonds_df)
+
+ # Display a Seaborn histogram showing all cuts of diamonds
+    with ui.navset_card_pill(id="tab2"):
+        with ui.nav_panel("Seaborn Diamond Histogram"):
+            @render.plot
+            def seaborn_histogram():
+               histplot = sns.histplot(data=diamonds_df, x="cut", hue="clarity", bins=input.seaborn_bin_count())
+               histplot.set_title("Seaborn Diamonds")
+               histplot.set_xlabel("Cut")
+               histplot.set_ylabel("Count")
+               sns.set_style('darkgrid')
+               return histplot 
+
+           
